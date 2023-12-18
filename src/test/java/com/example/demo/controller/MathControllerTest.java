ec41648550b69e68866d4fd90ce72e7f2640008b
MartheUwir <marthuwir@gmail.com>
2023-12-18 11:42:42 +0200

    calculator

---
+++ src/test/java/com/example/demo/controller/MathControllerTest.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+import rca.devopsexam.v1.dtos.DoMathRequestDto;
+import rca.devopsexam.v1.payload.ApiResponse;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class MathControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void doMathOperation_Success(){
+        DoMathRequestDto dto = new DoMathRequestDto(2, 5, "+");
+
+        ResponseEntity<ApiResponse> response = this.restTemplate.postForEntity("/api/v1/do_math",dto,ApiResponse.class);
+
+        assertEquals(200, response.getStatusCode().value());
+    }
+}
