645737627d6548bd9d12f6d9960ee5a1f5923b30
MartheUwir <marthuwir@gmail.com>
2023-12-18 11:10:16 +0200

    calculator

--- src/main/java/com/example/demo/exceptions/InvalidOperationException.java
+++ src/main/java/com/example/demo/exceptions/InvalidOperationException.java
@@ -1,4 +1,19 @@
 package com.example.demo.exceptions;
 
-public class InvalidOperationException {
-}
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.io.Serial;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+    public class InvalidOperationException extends Exception{
+        @Serial
+        private static final long serialVersionUID = 1L;
+
+        public InvalidOperationException(String message) {
+            super(message);
+        }
+
+    }
+
+
